cdc4edd22e72e5a7130af286deb28bb5bfd0bfab
Agadyr <kairambekovmadi@gmail.com>
2024-12-16 23:24:35 +0500

    some basic integrations in home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggenInc = { firstName: "Michael", lastName: "Kairambekov" }
+
   return (
-    <main>
-        sidebar
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggenInc}/>
         {children}
     </main>
   );
