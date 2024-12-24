1f971b219246b1bf754603434a4616a5ffcb6f8e
Agadyr <kairambekovmadi@gmail.com>
2024-12-24 21:01:00 +0500

    added pagination for transactions

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import { Pagination } from '@/components/Pagination';
 import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
@@ -19,6 +20,12 @@ const TransactionHistory = async({ searchParams: { id, page }}: SearchParamProps
 
   const account = await getAccount({ appwriteItemId });
 
+  const rowsPerPage = 10
+  const totalPages = Math.ceil(account.transactions.length / rowsPerPage)
+  const indexOfLastTransaction = currentPage * rowsPerPage
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage
+
+  const currentTransaction = account.transactions.slice(indexOfFirstTransaction, indexOfLastTransaction)
   return (
     <div className='transactions'>
       <div className='transactions-header'>
@@ -45,7 +52,12 @@ const TransactionHistory = async({ searchParams: { id, page }}: SearchParamProps
         </div>
 
         <section className='flex w-full flex-col gap-6'>
-          <TransactionsTable transactions={account?.transactions}/>
+          <TransactionsTable transactions={currentTransaction}/>
+          {totalPages > 1 && (
+            <div className="my-4 w-full">
+              <Pagination totalPages={totalPages} page={currentPage}/>
+            </div>
+          )}
         </section>
       </div>
     </div>
--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -4,10 +4,17 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import BankTabItem from "./BankTabItem";
 import BankInfo from "./BankInfo";
 import TransactionsTable from "./TransactionsTable";
+import { Pagination } from "./Pagination";
 
 const RecentTransactions = ({ accounts, transactions = [], appwriteItemId, page = 1 }: RecentTransactionsProps) => {
   const memoizedAccounts = useMemo(() => accounts, [accounts]);
-  const memoizedTransactions = useMemo(() => transactions, [transactions]);
+
+  const rowsPerPage = 10
+  const totalPages = Math.ceil(transactions.length / rowsPerPage)
+  const indexOfLastTransaction = page * rowsPerPage
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage
+
+  const currentTransaction = transactions.slice(indexOfFirstTransaction, indexOfLastTransaction)
   return (
     <section className="recent-transactions">
       <header className="flex items-center justify-between">
@@ -28,7 +35,14 @@ const RecentTransactions = ({ accounts, transactions = [], appwriteItemId, page 
         {memoizedAccounts.map((account: Account) => (
           <TabsContent value={account.appwriteItemId} key={account.id} className="space-y-4">
             <BankInfo key={account.id} account={account} appwriteItemId={appwriteItemId} type="full" />
-            <TransactionsTable transactions={memoizedTransactions} />
+            <TransactionsTable transactions={currentTransaction} />
+            
+            {totalPages > 1 && (
+              <div className="my-4 w-full">
+                <Pagination totalPages={totalPages} page={page}/>
+              </div>
+            )}
+
           </TabsContent>
         ))}
       </Tabs>
